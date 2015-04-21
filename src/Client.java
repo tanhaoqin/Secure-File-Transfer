6cd13b4e3a564646364666a39c5165bf2a061b45
tanhaoqin <haoqin_tan@mymail.sutd.edu.sg>
2015-04-21 10:10:05 +0800

    Added initial code for assignment

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,66 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 
 public class Client {
 
+	private static final String LOCALHOST = "127.0.0.1";
+	
+	public static final String FILE_TRANSFER_START = "FILE_TRANSFER_START";
+	public static final String FILE_TRANSFER_END = "FILE_TRANSFER_END";
+	
+	private static Socket socket;
+
+	private static InputStream in;
+
+	private static OutputStream out;
+	
+	byte[] buffer;
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		socket = new Socket(LOCALHOST, 4321);
+		System.out.println("Client connected");
+		in = socket.getInputStream();
+		out = socket.getOutputStream();
+		
+		System.out.println(System.getProperty("user.dir"));
+		
+		uploadFile(new File("certs//server_Tan Hao Qin.csr"));
+		
+	}
+	
+	public static void uploadFile(File file) throws IOException{
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(out);
+		PrintWriter printWriter = new PrintWriter(out);
+		
+		printWriter.write(FILE_TRANSFER_START+"\n");
+		printWriter.flush();
+		printWriter.write(file.getName()+"\n");
+		printWriter.flush();
+		System.out.println(Long.toString(file.length())+"\n");
+		printWriter.write(Long.toString(file.length())+"\n");
+		printWriter.flush();
+
+		
+		int a = 0;
+		while(a != -1){
+			a = bufferedInputStream.read();
+			bufferedOutputStream.write(a);
+		}
+		in.close();
+		out.close();
+	}
+	
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -49,9 +51,31 @@ class ClientHandler implements Runnable{
 	public void run() {
 		String input;
 		try {
-			while((input = in.readLine()) != END_PROTOCOL){
-				if(input.equals(IDENTITY_PROTOCOL)){
+			while((END_PROTOCOL).equals(input = in.readLine())){
+				System.out.println(input);
+				if(input.equals(Client.FILE_TRANSFER_START)){
 					
+					String fileName = "server//"+in.readLine();
+					System.out.println("File name: "+fileName);
+					
+					int length = Integer.parseInt(in.readLine());
+					int count = 0;
+					
+					byte[] byteArray = new byte[length];
+					
+					BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+
+					int a = 0;
+					while(a != -1 && count < length){
+						System.out.println(a);
+						a = bufferedInputStream.read();
+						byteArray[count] = (byte) a;
+						count++;
+					}
+					
+					FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+					fileOutputStream.write(byteArray);
+					fileOutputStream.close();
 				}
 			}
 		} catch (IOException e) {
